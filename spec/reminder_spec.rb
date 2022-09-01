4038dadb36e4118c1cc4ceae92dc0d7e0422f825
Patryk <patrykokpara@hotmail.com>
2022-09-01 16:29:43 +0100

    Added/ tested an error  in the Reminder class.

--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -3,8 +3,6 @@ require 'reminder'
 RSpec.describe Reminder do
     it "reminds user to perform a task" do
         reminder = Reminder.new("Tom")
-        reminder.remind_me_to("Go to therapy")
-        result = reminder.remind()
-        expect(result).to eq "Go to therapy, Tom!"
+        expect{ reminder.remind() }.to raise_error "No reminder set!"
     end
 end
