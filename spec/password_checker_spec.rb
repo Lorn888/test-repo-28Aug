0caf8d4e5263a6cea83e99ddcfb02ffa06c08d18
Patryk <patrykokpara@hotmail.com>
2022-09-01 16:57:31 +0100

    Added a test to Password_checker class

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -1,8 +1,13 @@
 require 'password_checker'
 
 RSpec.describe PasswordChecker do
-    it "Checks if password is longer than 8 chars" do
+    it "Checks if throws an error if passowrd is shorter than 8 chars" do
         pass = PasswordChecker.new
         expect{ pass.check("ok") }.to raise_error "Invalid password, must be 8+ characters."
+    end
+    it "Returns true if passowrd is 8 or more chars long" do
+        pass = PasswordChecker.new
+        result = pass.check("Newpassowrd1")
+        expect(result).to eq true
     end
 end
